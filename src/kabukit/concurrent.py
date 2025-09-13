d8db50203f23140e9d71bd57cefc7c98f4aef884
Daizu <daizutabi@gmail.com>
2025-09-13 14:57:55 +0900

    修正: fetch関数を削除し、関連するインポートを整理

--- src/kabukit/concurrent.py
+++ src/kabukit/concurrent.py
@@ -3,40 +3,8 @@ from __future__ import annotations
 import asyncio
 from typing import TYPE_CHECKING
 
-import polars as pl
-from polars import DataFrame
-
 if TYPE_CHECKING:
     from collections.abc import AsyncIterator, Awaitable, Callable, Iterable
-
-
-# async def fetch[T](
-#     function: Callable[[T], Awaitable[DataFrame]],
-#     iterable: Iterable[T],
-#     /,
-#     max_concurrency: int = 24,
-#     *,
-#     bar: bool = False,
-#     total: int | None = None,
-# ) -> DataFrame:
-#     """Fetch data concurrently using the provided function."""
-#     if total is None:
-#         iterable = list(iterable)
-#         total = len(iterable)
-
-#     ait = collect_async(function, iterable, max_concurrency=max_concurrency)
-
-#     # if bar:
-#     #     from marimo._plugins.stateless.status._progress import progress_bar
-
-#     #     ait = progress_bar(ait, total=total)
-
-#     dfs = [df async for df in ait if not df.is_empty()]
-
-#     if dfs:
-#         return pl.concat(dfs)
-
-#     return DataFrame()
 
 
 async def collect[R](
