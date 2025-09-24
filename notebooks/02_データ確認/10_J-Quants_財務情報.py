6a1f26142bf9f9577c9204ac2621d7c3cf46f472
Daizu <daizutabi@gmail.com>
2025-09-25 08:02:52 +0900

    関数の引数にplを追加し、データ処理のロジックを改善。各書類で有効なカラムと真偽値カラムの取得処理を整理。

--- notebooks/02_データ確認/10_J-Quants_財務情報.py
+++ notebooks/02_データ確認/10_J-Quants_財務情報.py
@@ -22,7 +22,7 @@ def _():
     import polars as pl
     from polars import col as c
     from kabukit import Statements
-    return Statements, c, mo
+    return Statements, c, mo, pl
 
 
 @app.cell
@@ -60,27 +60,59 @@ def _(c, data):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""## 各書類で有効なカラム""")
-    return
+    mo.md(
+        r"""
+    ## 各書類で有効なカラム
 
-
-@app.cell
-def _(data):
-    data.filter(Code="33500").select(
-        "Date", "Code", "TypeOfDocument", "TotalAssets", "Equity", "BookValuePerShare", "^.*NumberOf.*$"
+    ### すべての書類でnullを含まないカラム
+    """
     )
     return
 
 
 @app.cell
-def _():
-    16965000000/36268334
-    return
+def _(data):
+    common_columns = [s.name for s in data if s.is_not_null().all() and s.name != "TypeOfDocument"]
+    common_columns
+    return (common_columns,)
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""## 決算短信""")
+    mo.md(r"""### 真偽値カラム""")
+    return
+
+
+@app.cell
+def _(data, pl):
+    bool_columns = [s.name for s in data if s.dtype == pl.Boolean]
+    bool_columns
+    return (bool_columns,)
+
+
+@app.cell
+def _(bool_columns, common_columns, data, pl):
+    pct = (
+        data.drop(common_columns, bool_columns)
+        .group_by("TypeOfDocument")
+        .agg(pl.len(), pl.exclude(common_columns, "Time").is_not_null().sum())
+        .with_columns((pl.exclude("TypeOfDocument", "len") / pl.col("len") * 100).round(1))
+        .sort("TypeOfDocument")
+    )
+    pct
+    return (pct,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""### 決算短信""")
+    return
+
+
+@app.cell
+def _(c, pct):
+    _df = pct.filter(c.TypeOfDocument.str.contains("1QFinancial"))
+    _df.select(s.name for s in _df if s.name == "TypeOfDocument" or s.max() > 0)
     return
 
 
