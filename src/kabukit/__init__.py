f624d3c2bc103907414fa9227f727ae7f2b868e6
Daizu <daizutabi@gmail.com>
2025-09-27 02:11:27 +0000

    Listを__all__に追加しました。

--- src/kabukit/__init__.py
+++ src/kabukit/__init__.py
@@ -1,7 +1,8 @@
 from .core.info import Info
+from .core.list import List
 from .core.prices import Prices
 from .core.statements import Statements
 from .edinet.client import EdinetClient
 from .jquants.client import JQuantsClient
 
-__all__ = ["EdinetClient", "Info", "JQuantsClient", "Prices", "Statements"]
+__all__ = ["EdinetClient", "Info", "JQuantsClient", "List", "Prices", "Statements"]
