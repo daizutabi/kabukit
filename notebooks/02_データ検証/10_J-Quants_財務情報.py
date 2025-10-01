3af5a5a4d51ba43a683ed6590d7050f2820c087b
Daizu <daizutabi@gmail.com>
2025-10-01 22:24:26 +0000

    marimoのバージョンを0.16.3から0.16.4に更新し、データ取得ロジックを整理しました。また、価格データと財務データの結合処理を追加しました。

--- notebooks/02_データ検証/10_J-Quants_財務情報.py
+++ notebooks/02_データ検証/10_J-Quants_財務情報.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.16.3"
+__generated_with = "0.16.4"
 app = marimo.App(width="medium")
 
 
@@ -17,25 +17,61 @@ def _():
     import polars as pl
     from polars import col as c
     from kabukit import Statements, Prices
-    return Prices, Statements, c, date, mo
+    return Prices, Statements, c, date, mo, pl
 
 
 @app.cell
 def _(Prices, Statements):
     statements = Statements.read()
     prices = Prices.read()
-    return (statements,)
+    return prices, statements
 
 
 @app.cell
 def _(c, date, statements):
-    statements.number_of_shares().filter(c.Code == "39970", c.Date > date(2025, 1, 1))
+    statement_df = (
+        statements.number_of_shares()
+        .filter(c.Code == "39970", c.Date > date(2025, 1, 1))
+        .select("Date", "Code", Shares=c.TotalShares - c.TreasuryShares)
+    )
+    statement_df
+    return (statement_df,)
+
+
+@app.cell
+def _(c, date, prices):
+    prices_df = prices.data.filter(c.Code == "39970", c.Date > date(2025, 1, 1)).select(
+        "Date", "Code", "AdjustmentFactor", "Volume", "RawVolume"
+    )
+    prices_df.tail()
+    return (prices_df,)
+
+
+@app.cell
+def _(c, prices_df):
+    prices_df.filter(c.AdjustmentFactor != 1)
     return
 
 
 @app.cell
-def _(statements):
-    statements.data.filter(Code="39970")
+def _(prices_df, statement_df):
+    daily_df = prices_df.join_asof(
+        statement_df.rename({"Date": "ReportDate", "Shares": "ReportedShares"}),
+        left_on="Date",
+        right_on="ReportDate",
+        by="Code",
+        check_sortedness=False,
+    )
+    daily_df.tail()
+    return (daily_df,)
+
+
+@app.cell
+def _(c, daily_df, pl):
+    adjusted_df = daily_df.with_columns(
+        (1.0 / c.AdjustmentFactor).cum_prod().over("Code", "ReportDate").alias("CumulativeRatio")
+    ).with_columns((c.ReportedShares * c.CumulativeRatio).round(0).cast(pl.Int64))
+    adjusted_df
     return
 
 
