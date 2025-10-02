dd8b2955abacab457fc417c4fc8f8124dcc3856f
Daizu <daizutabi@gmail.com>
2025-10-02 04:34:35 +0000

    Pricesフィクスチャにステートメントを追加し、調整後の株式数を計算するロジックを改善しました。また、テストケースを整理し、データ取得方法を最適化しました。

--- tests/validation/cross/test_adjusted_shares.py
+++ tests/validation/cross/test_adjusted_shares.py
@@ -1,4 +1,8 @@
+from datetime import date
+
+import pytest
 import pytest_asyncio
+from polars import DataFrame
 from polars import col as c
 
 from kabukit.core.prices import Prices
@@ -8,16 +12,17 @@ from tests.validation.conftest import pytestmark  # noqa: F401
 
 
 @pytest_asyncio.fixture(scope="module")
-async def prices() -> Prices:
+async def prices(statements: Statements) -> Prices:
     codes = ["3350", "6200", "3399", "7187", "6542", "3816", "4923", "3997"]
     data = await fetch("prices", codes)
-    return Prices(data)
+    return Prices(data).with_adjusted_shares(statements)
 
 
-def test_adjusted_shares(prices: Prices, statements: Statements) -> None:
-    data = prices.with_adjusted_shares(statements).data
+@pytest.fixture(scope="module")
+def data(prices: Prices) -> DataFrame:
+    data = prices.data
 
-    df = (
+    return (
         data.with_columns(
             MarketCap=c.RawClose * c.AdjustedTotalShares,
         )
@@ -35,18 +40,77 @@ def test_adjusted_shares(prices: Prices, statements: Statements) -> None:
         )
     )
 
-    x = df["Ratio"].mean()
+
+def test_adjusted_shares_ratio_min(data: DataFrame) -> None:
+    x = data["Ratio"].mean()
     assert isinstance(x, float)
     assert x < 0.05
 
-    x = df["Ratio"].max()
+
+def test_adjusted_shares_ratio_max(data: DataFrame) -> None:
+    x = data["Ratio"].max()
     assert isinstance(x, float)
     assert x < 0.25
 
-    x = df["RatioRatio"].min()
+
+def test_adjusted_shares_ratio_ratio_min(data: DataFrame) -> None:
+    x = data["RatioRatio"].min()
     assert isinstance(x, float)
     assert x > 0.995
 
-    x = df["RatioRatio"].max()
+
+def test_adjusted_shares_ratio_ratio_max(data: DataFrame) -> None:
+    x = data["RatioRatio"].max()
     assert isinstance(x, float)
     assert x < 1.05
+
+
+def test_adjusted_shares_3997(prices: Prices) -> None:
+    df = prices.filter(c.Code == "39970").data
+    a = df.filter(c.Date == date(2025, 9, 26)).row(0, named=True)
+    assert a["AdjustmentFactor"] == 1
+    assert a["RawClose"] == 3610
+    assert a["AdjustedTotalShares"] == 3901800
+
+    b = df.filter(c.Date == date(2025, 9, 29)).row(0, named=True)
+    assert b["AdjustmentFactor"] == 0.1
+    assert b["RawClose"] == 353
+    assert b["AdjustedTotalShares"] == 39018000
+
+    x = a["RawClose"] * a["AdjustedTotalShares"]
+    y = b["RawClose"] * b["AdjustedTotalShares"]
+    assert x / y == 361 / 353
+
+
+def test_adjusted_shares_3350(prices: Prices) -> None:
+    df = prices.filter(c.Code == "33500").data
+    a = df.filter(c.Date == date(2024, 7, 29)).row(0, named=True)
+    assert a["AdjustmentFactor"] == 1
+    assert a["RawClose"] == 201
+    assert a["AdjustedTotalShares"] == 114692187
+
+    b = df.filter(c.Date == date(2024, 7, 30)).row(0, named=True)
+    assert b["AdjustmentFactor"] == 10
+    assert b["RawClose"] == 1510
+    assert b["AdjustedTotalShares"] == 11469219
+
+    x = a["RawClose"] * a["AdjustedTotalShares"]
+    y = b["RawClose"] * b["AdjustedTotalShares"]
+    assert round(x / y, 7) == round(201 / 151, 7)
+
+
+def test_adjusted_shares_6200(prices: Prices) -> None:
+    df = prices.filter(c.Code == "62000").data
+    a = df.filter(c.Date == date(2022, 12, 28)).row(0, named=True)
+    assert a["AdjustmentFactor"] == 1
+    assert a["RawClose"] == 3095
+    assert a["AdjustedTotalShares"] == 42621500
+
+    b = df.filter(c.Date == date(2022, 12, 29)).row(0, named=True)
+    assert b["AdjustmentFactor"] == 0.5
+    assert b["RawClose"] == 1534
+    assert b["AdjustedTotalShares"] == 85243000
+
+    x = a["RawClose"] * a["AdjustedTotalShares"]
+    y = b["RawClose"] * b["AdjustedTotalShares"]
+    assert x / y == 3095 / 2 / 1534
