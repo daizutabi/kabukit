88a06e84009c4593d1b0ff5203eaaa53b26b4706
Daizu <daizutabi@gmail.com>
2025-10-04 10:05:08 +0000

    予想利益のテストを更新し、3997のデータを追加。テストケースのパラメータを修正し、コードの整合性を向上。

--- tests/validation/cross/test_dividend.py
+++ tests/validation/cross/test_dividend.py
@@ -14,37 +14,38 @@ from tests.validation.conftest import pytestmark  # noqa: F401
 async def prices(statements: Statements) -> Prices:
     codes = ["7203", "3997"]
     data = await fetch("prices", codes)
-    return Prices(data).with_forecast_profit(statements)
+    return Prices(data).with_forecast_dividend(statements)
 
 
 @pytest.mark.parametrize(
-    ("d", "profit"),
+    ("d", "dividend"),
     [
-        (date(2025, 5, 7), 4520000000000),
-        (date(2025, 5, 8), 3100000000000),
-        (date(2025, 10, 3), 2660000000000),
+        (date(2025, 5, 7), 1193416845132),
+        (date(2025, 5, 8), 1239634634003),
+        (date(2025, 10, 3), 1238179234651),
     ],
 )
-def test_forecast_profit_7203(
+def test_forecast_dividend_7203(
     prices: Prices,
     d: date,
-    profit: float,
+    dividend: float,
 ) -> None:
     df = prices.data.filter(c.Code == "72030", c.Date == d)
-    assert df.item(0, "ForecastProfit") == profit
+    assert df.item(0, "ForecastDividend") == dividend
 
 
 @pytest.mark.parametrize(
-    ("d", "profit"),
+    ("d", "dividend"),
     [
-        (date(2025, 1, 10), -123000000),
-        (date(2025, 10, 3), 100000000),
+        (date(2024, 12, 27), 69120539),
+        (date(2025, 5, 12), 68917988),
+        (date(2025, 10, 3), 68965517),
     ],
 )
-def test_forecast_profit_3997(
+def test_forecast_dividend_3997(
     prices: Prices,
     d: date,
-    profit: float,
+    dividend: float,
 ) -> None:
     df = prices.data.filter(c.Code == "39970", c.Date == d)
-    assert df.item(0, "ForecastProfit") == profit
+    assert df.item(0, "ForecastDividend") == dividend
