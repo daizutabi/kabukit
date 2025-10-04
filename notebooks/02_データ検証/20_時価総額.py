6a2b5006bfbe6d4634d29182a44495484ba83f3b
Daizu <daizutabi@gmail.com>
2025-10-04 20:17:47 +0000

    新しい財務情報検証用のノートブックを追加し、時価総額、純資産、予想純利益、予想配当の計算ロジックを実装

---
+++ notebooks/02_データ検証/20_時価総額.py
@@ -0,0 +1,51 @@
+import marimo
+
+__generated_with = "0.16.5"
+app = marimo.App(width="medium")
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        """
+    # J-Quantsで取得できる財務情報を検証する
+
+    <https://pdf.irpocket.com/C3997/bffO/ugVK/I9Yg.pdf>
+    """
+    )
+    return
+
+
+@app.cell
+def _():
+    import marimo as mo
+    from datetime import date
+    import polars as pl
+    from polars import col as c
+    from kabukit import Statements, Prices
+    return Prices, Statements, c, mo
+
+
+@app.cell
+def _(Prices, Statements):
+    statements = Statements.read()
+    prices = Prices.read()
+    return prices, statements
+
+
+@app.cell
+def _(c, prices, statements):
+    prices.with_adjusted_shares(statements).with_market_cap().filter(
+        c.Code == "39970"
+        # c.Code == "72030"
+    ).data.select("Date", "RawClose", "AdjustedIssuedShares", "AdjustedTreasuryShares", "MarketCap")
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
