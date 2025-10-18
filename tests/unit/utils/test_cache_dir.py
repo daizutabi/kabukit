935ccab154e12e10487b4f209623f03c4af92c3a
Daizu <daizutabi@gmail.com>
2025-10-18 15:43:45 +0900

    fix: 正規表現のコンパイルを削除し、直接マッチングを使用するように修正

--- tests/unit/utils/test_cache_dir.py
+++ tests/unit/utils/test_cache_dir.py
@@ -13,8 +13,8 @@ def test_get_cache_dir_linux() -> None:
     from kabukit.utils.config import get_cache_dir
 
     result = get_cache_dir()
-    pattern = re.compile(r"^/home/\S+/\.cache/kabukit$")
-    assert pattern.match(str(result))
+    pattern = r"^/home/\S+/\.cache/kabukit$"
+    assert re.match(pattern, str(result))
 
 
 @pytest.mark.skipif(sys.platform != "darwin", reason="This test is for macOS")
@@ -22,8 +22,8 @@ def test_get_cache_dir_darwin() -> None:
     from kabukit.utils.config import get_cache_dir
 
     result = get_cache_dir()
-    pattern = re.compile(r"^/Users/\S+/Library/Caches/kabukit$")
-    assert pattern.match(str(result))
+    pattern = r"^/Users/\S+/Library/Caches/kabukit$"
+    assert re.match(pattern, str(result))
 
 
 @pytest.mark.skipif(sys.platform != "win32", reason="This test is for Windows")
@@ -31,5 +31,5 @@ def test_get_cache_dir_windows() -> None:
     from kabukit.utils.config import get_cache_dir
 
     result = get_cache_dir()
-    pattern = re.compile(r"^C:\Users\S+\AppData\Local\kabukit\Cache$")
-    assert pattern.match(str(result))
+    pattern = r"^C:\\Users\S+\\AppData\\Locall\\kabukit\\Cache$"
+    assert re.match(pattern, str(result))
