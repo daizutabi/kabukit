4664112bb33b21bc0d99ffc17411aa35ff41c736
Daizu <daizutabi@gmail.com>
2025-10-22 14:19:24 +0900

    feat: `get_list`と`get_documents`を`edinet`モジュールに追加し、`__all__`を更新

--- src/kabukit/__init__.py
+++ src/kabukit/__init__.py
@@ -5,7 +5,6 @@ from .domain.jquants.info import Info
 from .domain.jquants.prices import Prices
 from .domain.jquants.statements import Statements
 from .sources.edinet.client import EdinetClient
-from .sources.edinet.concurrent import get_documents, get_list
 from .sources.jquants.client import JQuantsClient
 from .sources.jquants.concurrent import (
     get_info,
@@ -21,9 +20,7 @@ __all__ = [
     "Prices",
     "Statements",
     "cache",
-    "get_documents",
     "get_info",
-    "get_list",
     "get_prices",
     "get_statements",
     "get_target_codes",
---
+++ src/kabukit/sources/edinet/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from .concurrent import get_documents, get_list
+
+__all__ = ["get_documents", "get_list"]
