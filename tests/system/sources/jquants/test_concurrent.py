c75776f5d4f13695dac7d86f8a055fc3e7425d96
Daizu <daizutabi@gmail.com>
2025-10-23 15:27:58 +0900

    test: `get_statements`と`get_prices`のテストを追加し、日付引数を使用して検証を強化

--- tests/system/sources/jquants/test_concurrent.py
+++ tests/system/sources/jquants/test_concurrent.py
@@ -1,8 +1,59 @@
 from __future__ import annotations
 
+import datetime
+
+import polars as pl
 import pytest
 
 pytestmark = pytest.mark.system
+
+
+@pytest.mark.asyncio
+async def test_get_calender() -> None:
+    from kabukit.sources.jquants.concurrent import get_calendar
+
+    df = await get_calendar()
+    df = df.filter(pl.col("IsHoliday"))
+    assert datetime.date(2026, 12, 31) in df["Date"]
+
+
+@pytest.mark.asyncio
+async def test_get_info() -> None:
+    from kabukit.sources.jquants.concurrent import get_info
+
+    df = await get_info()
+    assert df["Date"].n_unique() == 1
+    assert "72030" in df["Code"]
+    assert "99840" in df["Code"]
+
+
+@pytest.mark.asyncio
+async def test_get_info_with_code() -> None:
+    from kabukit.sources.jquants.concurrent import get_info
+
+    df = await get_info("7203")
+    assert "72030" in df["Code"]
+    assert df.height == 1
+
+
+@pytest.mark.asyncio
+async def test_get_info_with_date() -> None:
+    from kabukit.sources.jquants.concurrent import get_info
+
+    df = await get_info(date="2025-10-22")
+    assert df["Date"].unique().to_list() == [datetime.date(2025, 10, 22)]
+    assert "72030" in df["Code"]
+    assert "99840" in df["Code"]
+
+
+@pytest.mark.asyncio
+async def test_get_info_with_code_date() -> None:
+    from kabukit.sources.jquants.concurrent import get_info
+
+    df = await get_info("7203", "2025-10-22")
+    assert df["Date"].unique().to_list() == [datetime.date(2025, 10, 22)]
+    assert "72030" in df["Code"]
+    assert df.height == 1
 
 
 @pytest.mark.asyncio
@@ -23,11 +74,34 @@ async def test_get_target_codes_all_ends_with_zero() -> None:
 
 
 @pytest.mark.asyncio
+async def test_get_statements() -> None:
+    from kabukit.sources.jquants.concurrent import get_statements
+
+    df = await get_statements(max_items=3)
+    assert df["Code"].n_unique() == 3
+
+
+@pytest.mark.asyncio
 async def test_get_statements_with_code() -> None:
     from kabukit.sources.jquants.concurrent import get_statements
 
     df = await get_statements("7203")
+    assert df["Date"].n_unique() > 40
     assert df["Code"].unique().to_list() == ["72030"]
+
+
+@pytest.mark.asyncio
+async def test_get_statements_with_date() -> None:
+    from kabukit.sources.jquants.concurrent import get_statements
+
+    df = await get_statements(date="2025-10-21")
+    assert df["DisclosedDate"].unique().to_list() == [datetime.date(2025, 10, 21)]
+    assert df["Date"].unique().sort().to_list() == [
+        datetime.date(2025, 10, 21),
+        datetime.date(2025, 10, 22),
+    ]
+    assert "30910" in df["Code"]
+    assert "99140" in df["Code"]
 
 
 @pytest.mark.asyncio
@@ -47,15 +121,33 @@ async def test_get_statements_with_max_items() -> None:
 
 
 @pytest.mark.asyncio
-async def test_get_statements_without_codes() -> None:
-    from kabukit.sources.jquants.concurrent import get_statements
+async def test_get_prices() -> None:
+    from kabukit.sources.jquants.concurrent import get_prices
 
-    df = await get_statements(max_items=3)
+    df = await get_prices(max_items=3)
     assert df["Code"].n_unique() == 3
 
 
 @pytest.mark.asyncio
-async def test_get_prices() -> None:
+async def test_get_prices_with_code() -> None:
+    from kabukit.sources.jquants.concurrent import get_prices
+
+    df = await get_prices("7203")
+    assert df["Code"].unique().to_list() == ["72030"]
+    assert df["Date"].n_unique() > 2000
+
+
+@pytest.mark.asyncio
+async def test_get_prices_with_date() -> None:
+    from kabukit.sources.jquants.concurrent import get_prices
+
+    df = await get_prices(date="2025-10-22")
+    assert df["Date"].unique().to_list() == [datetime.date(2025, 10, 22)]
+    assert df["Code"].n_unique() > 3000
+
+
+@pytest.mark.asyncio
+async def test_get_prices_with_codes() -> None:
     from kabukit.sources.jquants.concurrent import get_prices
 
     df = await get_prices(["7203", "6758"])
@@ -67,20 +159,4 @@ async def test_get_prices_with_max_items() -> None:
     from kabukit.sources.jquants.concurrent import get_prices
 
     df = await get_prices(["7203", "6758"], max_items=1)
-    assert df["Code"].unique().to_list() == ["72030"]
-
-
-@pytest.mark.asyncio
-async def test_get_prices_without_codes() -> None:
-    from kabukit.sources.jquants.concurrent import get_prices
-
-    df = await get_prices(max_items=3)
-    assert df["Code"].n_unique() == 3
-
-
-@pytest.mark.asyncio
-async def test_get_prices_with_single_code() -> None:
-    from kabukit.sources.jquants.concurrent import get_prices
-
-    df = await get_prices("7203")
     assert df["Code"].unique().to_list() == ["72030"]
