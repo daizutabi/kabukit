61dcd468c9aff9584090676196538aed7f4e5851
Daizu <daizutabi@gmail.com>
2025-10-24 21:19:55 +0900

    feat: `test_get_jquants`に全銘柄取得オプションを追加し、テストを更新

--- tests/unit/cli/test_get_jquants.py
+++ tests/unit/cli/test_get_jquants.py
@@ -33,21 +33,25 @@ def mock_cli_prices(mocker: MockerFixture) -> AsyncMock:
     return mocker.patch("kabukit.cli.get.prices", new_callable=AsyncMock)
 
 
+@pytest.mark.parametrize("all_", [[], ["--all"]])
 @pytest.mark.parametrize("quiet", [[], ["-q"], ["--quiet"]])
 @pytest.mark.parametrize(("codes", "arg"), [([MOCK_CODE], MOCK_CODE), ([], None)])
 def test_get_jquants_with_code(
     mock_cli_info: AsyncMock,
     mock_cli_statements: AsyncMock,
     mock_cli_prices: AsyncMock,
+    all_: list[str],
     quiet: list[str],
     codes: list[str],
     arg: str | None,
 ) -> None:
-    result = runner.invoke(app, ["get", "jquants", *codes, *quiet])
+    result = runner.invoke(app, ["get", "jquants", *codes, *quiet, *all_])
 
     assert result.exit_code == 0
 
+    a = bool(all_)
     q = bool(quiet)
+
     mock_cli_info.assert_awaited_once_with(arg, quiet=q)
-    mock_cli_statements.assert_awaited_once_with(arg, quiet=q, max_items=None)
-    mock_cli_prices.assert_awaited_once_with(arg, quiet=q, max_items=None)
+    mock_cli_statements.assert_awaited_once_with(arg, all_=a, max_items=None, quiet=q)
+    mock_cli_prices.assert_awaited_once_with(arg, all_=a, max_items=None, quiet=q)
