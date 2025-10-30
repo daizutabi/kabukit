75d082c141a8a2a1fef9c0860cf59c8a1fbb8037
Daizu <daizutabi@gmail.com>
2025-10-30 16:29:01 +0900

    feat: test_quote_priceとtest_quote_previous_price関数を追加し、関連するテストを実装

--- tests/system/sources/yahoo/test_state.py
+++ tests/system/sources/yahoo/test_state.py
@@ -4,30 +4,6 @@ import re
 from typing import Any
 
 
-def test_quote_press_release(quote: dict[str, Any]) -> None:
-    x = quote["mainStocksPressReleaseSummary"]
-    assert len(x) == 2
-    assert "disclosedTime" in x
-    assert "summary" in x
-
-
-def test_quote_iter_press_release(quote: dict[str, Any]) -> None:
-    from kabukit.sources.yahoo.parser import iter_press_release
-
-    x = dict(iter_press_release(quote))
-    assert len(x) == 3
-
-
-def test_quote_performance(quote: dict[str, Any]) -> None:
-    x = quote["stockPerformance"]["summaryInfo"]
-    assert len(x) == 5
-    assert "potential" in x
-    assert "profitability" in x
-    assert "stability" in x
-    assert "summary" in x
-    assert "updateTime" in x
-
-
 def test_quote_price(quote: dict[str, Any]) -> None:
     x = quote["mainStocksPriceBoard"]["priceBoard"]
     assert len(x) == 27
@@ -42,3 +18,20 @@ def test_quote_previous_price(quote: dict[str, Any]) -> None:
     assert "previousPrice" in x
     assert "previousPriceDate" in x
     assert re.match(r"^\d+/\d+$", x["previousPriceDate"])
+
+
+def test_quote_press_release(quote: dict[str, Any]) -> None:
+    x = quote["mainStocksPressReleaseSummary"]
+    assert len(x) == 2
+    assert "disclosedTime" in x
+    assert "summary" in x
+
+
+def test_quote_performance(quote: dict[str, Any]) -> None:
+    x = quote["stockPerformance"]["summaryInfo"]
+    assert len(x) == 5
+    assert "potential" in x
+    assert "profitability" in x
+    assert "stability" in x
+    assert "summary" in x
+    assert "updateTime" in x
