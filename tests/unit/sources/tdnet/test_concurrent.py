8debf64bb06ef7b451089ca559fd706a89121dde
Daizu <daizutabi@gmail.com>
2025-10-31 19:28:02 +0900

    feat: mock_gather_getを使用してget_listのテストを改善し、冗長なコードを削除

--- tests/unit/sources/tdnet/test_concurrent.py
+++ tests/unit/sources/tdnet/test_concurrent.py
@@ -11,6 +11,8 @@ from kabukit.sources.tdnet import concurrent
 from kabukit.sources.tdnet.client import TdnetClient
 
 if TYPE_CHECKING:
+    from unittest.mock import AsyncMock
+
     from pytest_mock import MockerFixture
 
 pytestmark = pytest.mark.unit
@@ -27,23 +29,15 @@ async def test_get_list_single_date(mocker: MockerFixture) -> None:
     assert_frame_equal(result, mock_df)
 
 
-async def test_get_list_multiple_dates(mocker: MockerFixture) -> None:
+async def test_get_list_multiple_dates(mock_gather_get: AsyncMock) -> None:
     dates = [datetime.date(2023, 1, 1), datetime.date(2023, 1, 2)]
-    mock_df_concat = pl.DataFrame(
-        {
-            "Code": [1, 2],
-            "Date": [datetime.date(2023, 1, 1), datetime.date(2023, 1, 2)],
-        },
-    )
-    mock_concurrent_get = mocker.AsyncMock(return_value=mock_df_concat)
-    mocker.patch(
-        "kabukit.sources.tdnet.concurrent.gather.get",
-        new=mock_concurrent_get,
-    )
+
+    mock_df = pl.DataFrame({"Code": [1, 2], "Date": dates})
+    mock_gather_get.return_value = mock_df
 
     result = await concurrent.get_list(dates)
 
-    mock_concurrent_get.assert_awaited_once_with(
+    mock_gather_get.assert_awaited_once_with(
         TdnetClient,
         "list",
         dates,
@@ -52,47 +46,37 @@ async def test_get_list_multiple_dates(mocker: MockerFixture) -> None:
         progress=None,
         callback=None,
     )
-    assert_frame_equal(result, mock_df_concat.sort("Code", "Date"))
+    assert_frame_equal(result, mock_df.sort("Code", "Date"))
 
 
-async def test_get_list_no_dates_specified(mocker: MockerFixture) -> None:
-    available_dates = [datetime.date(2023, 1, 1), datetime.date(2023, 1, 2)]
-    mock_get_dates_method = mocker.AsyncMock(return_value=available_dates)
+async def test_get_list_no_dates_specified(
+    mocker: MockerFixture,
+    mock_gather_get: AsyncMock,
+) -> None:
+    dates = [datetime.date(2023, 1, 1), datetime.date(2023, 1, 2)]
+    mock_get_dates_method = mocker.AsyncMock(return_value=dates)
     mocker.patch.object(TdnetClient, "get_dates", new=mock_get_dates_method)
 
-    mock_df_concat = pl.DataFrame(
-        {
-            "Code": [1, 2],
-            "Date": [datetime.date(2023, 1, 1), datetime.date(2023, 1, 2)],
-        },
-    )
-    mock_concurrent_get = mocker.AsyncMock(return_value=mock_df_concat)
-    mocker.patch(
-        "kabukit.sources.tdnet.concurrent.gather.get",
-        new=mock_concurrent_get,
-    )
+    mock_df = pl.DataFrame({"Code": [1, 2], "Date": dates})
+    mock_gather_get.return_value = mock_df
 
     result = await concurrent.get_list(None)
 
     mock_get_dates_method.assert_awaited_once()
-    mock_concurrent_get.assert_awaited_once_with(
+    mock_gather_get.assert_awaited_once_with(
         TdnetClient,
         "list",
-        available_dates,
+        dates,
         max_items=None,
         max_concurrency=None,
         progress=None,
         callback=None,
     )
-    assert_frame_equal(result, mock_df_concat.sort("Code", "Date"))
+    assert_frame_equal(result, mock_df.sort("Code", "Date"))
 
 
-async def test_get_list_returns_empty_dataframe(mocker: MockerFixture) -> None:
-    mock_concurrent_get = mocker.AsyncMock(return_value=pl.DataFrame())
-    mocker.patch(
-        "kabukit.sources.tdnet.concurrent.gather.get",
-        new=mock_concurrent_get,
-    )
+async def test_get_list_returns_empty_dataframe(mock_gather_get: AsyncMock) -> None:
+    mock_gather_get.return_value = pl.DataFrame()
 
     result = await concurrent.get_list([datetime.date(2023, 1, 1)])
 
