82ade9da919324f791f7ad415010754bd1b2090a
Daizu <daizutabi@gmail.com>
2025-11-01 09:13:31 +0900

    refactor: テスト関数をquoteからstateにリネームし、iter_indexを追加

--- tests/system/sources/yahoo/test_parser.py
+++ tests/system/sources/yahoo/test_parser.py
@@ -4,6 +4,7 @@ import datetime
 from typing import Any
 
 from kabukit.sources.yahoo.parser import (
+    iter_index,
     iter_performance,
     iter_press_release,
     iter_previous_price,
@@ -11,32 +12,45 @@ from kabukit.sources.yahoo.parser import (
 )
 
 
-def test_quote_iter_price(quote: dict[str, Any]) -> None:
-    x = dict(iter_price(quote))
+def test_state_iter_price(state: dict[str, Any]) -> None:
+    x = dict(iter_price(state))
     assert len(x) == 3
     assert isinstance(x["Price"], float)
     assert isinstance(x["PriceDate"], datetime.date)
     assert isinstance(x["PriceTime"], datetime.time)
 
 
-def test_quote_iter_previous_price(quote: dict[str, Any]) -> None:
-    x = dict(iter_previous_price(quote))
+def test_state_iter_previous_price(state: dict[str, Any]) -> None:
+    x = dict(iter_previous_price(state))
     assert len(x) == 3
     assert isinstance(x["PreviousPrice"], float)
     assert isinstance(x["PreviousPriceDate"], datetime.date)
     assert isinstance(x["PreviousPriceTime"], datetime.time)
 
 
-def test_quote_iter_press_release(quote: dict[str, Any]) -> None:
-    x = dict(iter_press_release(quote))
+def test_state_iter_index(state: dict[str, Any]) -> None:
+    x = dict(iter_index(state))
+    assert len(x) == 8
+    assert isinstance(x["IssuedShares"], int)
+    assert isinstance(x["IssuedSharesDate"], datetime.date)
+    assert isinstance(x["BookValuePerShare"], float)
+    assert isinstance(x["BookValuePerShareDate"], datetime.date)
+    assert isinstance(x["EarningsPerShare"], float)
+    assert isinstance(x["EarningsPerShareDate"], datetime.date)
+    assert isinstance(x["DividendPerShare"], float)
+    assert isinstance(x["DividendPerShareDate"], datetime.date)
+
+
+def test_state_iter_press_release(state: dict[str, Any]) -> None:
+    x = dict(iter_press_release(state))
     assert len(x) == 3
     assert isinstance(x["PressReleaseSummary"], str)
     assert isinstance(x["PressReleaseDate"], datetime.date)
     assert isinstance(x["PressReleaseTime"], datetime.time)
 
 
-def test_quote_iter_performance(quote: dict[str, Any]) -> None:
-    x = dict(iter_performance(quote))
+def test_state_iter_performance(state: dict[str, Any]) -> None:
+    x = dict(iter_performance(state))
     assert len(x) == 6
     assert isinstance(x["PerformanceSummary"], str)
     assert isinstance(x["PerformancePotential"], str)
